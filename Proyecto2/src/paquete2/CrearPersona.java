6e7525192df3476f4782e85ef861b081b780180e
profesor <profesor@DESKTOP-ALAO8T3>
2025-03-17 18:45:31 +0100

    Creamos objeto persona1 de la Clase Persona

--- Proyecto2/src/paquete2/CrearPersona.java
+++ Proyecto2/src/paquete2/CrearPersona.java
@@ -3,7 +3,11 @@ package paquete2;
 public class CrearPersona {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Persona persona1=new Persona("María", 30);
+		persona1.mostrarInfomacion();
+		
+		
 
 	}
 
