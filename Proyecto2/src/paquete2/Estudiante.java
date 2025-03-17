900fdec48cdf6d9112f74e87bd8646ae8e46ce67
profesor <profesor@DESKTOP-ALAO8T3>
2025-03-17 18:41:29 +0100

    Creamos la clase estudiante que hereda de Persona.

---
+++ Proyecto2/src/paquete2/Estudiante.java
@@ -0,0 +1,29 @@
+package paquete2;
+
+public class Estudiante extends Persona {
+	private String matricula;
+	
+	public Estudiante(String nombre, int edad, String matricula) {
+		super(nombre, edad);
+		this.matricula=matricula;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	
+	@Override
+	public void mostrarInfomacion(){
+		super.mostrarInfomacion();
+		System.out.println("Matrícula: "+matricula);
+	}
+	
+	
+	
+
+}
